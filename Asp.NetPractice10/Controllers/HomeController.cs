c87ddab4853e641a45dc68b53e2f99ea8bb7b5a7
Fantather <dirivolkova@gmail.com>
2026-02-16 20:47:10 +0200

    Не видно ссылки

--- Asp.NetPractice10/Controllers/HomeController.cs
+++ Asp.NetPractice10/Controllers/HomeController.cs
@@ -30,5 +30,68 @@ namespace Asp.NetPractice10.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id != null)
+            {
+                var currentUser = await _context.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (currentUser != null)
+                {
+                    return View(currentUser);
+                }
+            }
+            return NotFound();
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                var currentUser = await _context.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (currentUser != null)
+                {
+                    return View(currentUser);
+                }
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> ConfirmDelete(int? id)
+        {
+            if (id != null)
+            {
+                var currentUser = await _context.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (currentUser != null)
+                {
+                    return View(currentUser);
+                }
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id != null)
+            {
+                var currentUser = await _context.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (currentUser != null)
+                {
+                    _context.Users.Remove(currentUser);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return NotFound();
+        }
     }
 }
